47a2048b58669af792e832b592ecd99209dfee0d
nagiyu <otonagiyu@gmail.com>
2024-09-04 10:24:06 +0900

    chore: Add session controllers for different session behaviors

---
+++ Framework/Controllers/ReadOnlySessionController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using Framework.Helpers;
+
+namespace Framework.Controllers
+{
+    [SessionState(System.Web.SessionState.SessionStateBehavior.ReadOnly)]
+    public class ReadOnlySessionController : Controller
+    {
+        // セッションのカウンターを取得する
+        public JsonResult GetSessionCounter()
+        {
+            return SessionHelper.GetSessionCounter(Session);
+        }
+
+        // セッションのカウンターをインクリメントする
+        public JsonResult IncrementSessionCounter()
+        {
+            return SessionHelper.IncrementSessionCounter(Session);
+        }
+    }
+}
---
+++ Framework/Helpers/SessionHelper.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Framework.Helpers
+{
+    public class SessionHelper
+    {
+        // セッションのカウンターを取得する
+        public static JsonResult GetSessionCounter(HttpSessionStateBase session)
+        {
+            // セッションのカウンターを取得
+            var counter = 0;
+            if (session["Counter"] != null)
+            {
+                counter = (int)session["Counter"];
+            }
+
+            // JSON でカウンターを返す
+            return new JsonResult { Data = new { counter = counter }, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+        }
+
+        // セッションのカウンターをインクリメントする
+        public static JsonResult IncrementSessionCounter(HttpSessionStateBase session)
+        {
+            // セッションのカウンターを取得
+            var counter = 0;
+            if (session["Counter"] != null)
+            {
+                counter = (int)session["Counter"];
+            }
+
+            // セッションのカウンターをインクリメント
+            counter++;
+
+            // セッションのカウンターを更新
+            session["Counter"] = counter;
+
+            // JSON でインクリメントしたカウンターを返す
+            return new JsonResult { Data = new { counter = counter }, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+        }
+    }
+}
